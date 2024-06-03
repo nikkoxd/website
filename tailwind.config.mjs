d404beb98bfe2f4a3d50da3f1e8b5345c150a98f
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-06-04 01:34:13 +0400

    feat: :sparkles: make basic home layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      backgroundImage: {
+        noise: "url(noise.png)",
+      },
       fontFamily: {
         mplus: "'M PLUS Rounded 1c', system, sans-serif",
       },
