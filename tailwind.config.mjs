bbca5fe07c240433f1422eaa78a810b17d0d3f4d
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-06-04 01:01:40 +0400

    feat: :lipstick: add custom fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mplus: "'M PLUS Rounded 1c', system, sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
